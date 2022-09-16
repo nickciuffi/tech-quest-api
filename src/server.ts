67b8056872e7c99e6976aa783617061c88d833f2
nicolas ciuffi <nicolasciuffi1@gmail.com>
2022-09-16 09:49:18 -0300

    now it will work

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 // eslint-disable-next-line
+import Knex from './knex/config/database';
 import app from './app';
 
 const port = process.env.APP_PORT;
-app.listen(port, () => {
+app.listen(port, async () => {
+  const resp = await Knex('Questionaries');
   console.log(`Server started in port ${port}`);
+  console.log(resp);
 });
