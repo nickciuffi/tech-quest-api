aed60a2404e3e1b56b5452d9b65b846a3c3be187
nicolas ciuffi <nicolasciuffi1@gmail.com>
2022-09-16 10:07:10 -0300

    last knexjs changwe

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,10 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }// eslint-disable-next-line
+var _database = require('./knex/config/database'); var _database2 = _interopRequireDefault(_database);
 var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
 const port = process.env.APP_PORT;
-_app2.default.listen(port, () => {
+_app2.default.listen(port, async () => {
+  const resp = await _database2.default.call(void 0, 'Questionaries');
   console.log(`Server started in port ${port}`);
+  console.log(resp);
 });
--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,15 @@
 // Update with your config settings.
-
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
 module.exports = {
 
   client: 'mysql2',
   connection: {
-    database: process.env.DATABASE,
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
+    database: 'db_tech_quest',
+    user: 'nickciuffi',
+    password: 'Niisaricris1',
   },
   pool: {
     min: 2,
